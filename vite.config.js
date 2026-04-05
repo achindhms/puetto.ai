cd7d23efa9178fa4831891770bfe0f318fc3734e
Achindh M S <achindhms@Achindhs-MacBook-Air.local>
2026-04-05 13:33:50 +0530

    fix: sitemap dynamic routes

--- vite.config.js
+++ vite.config.js
@@ -2,12 +2,14 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import sitemap from 'vite-plugin-sitemap'
 
+const routes = ['/', '/blog', '/blog/1', '/blog/2', '/blog/3', '/blog/4']
+
 export default defineConfig({
   plugins: [
     react(),
     sitemap({
       hostname: 'https://www.puetto.com',
-      routes: ['/', '/blog', '/blog/1', '/blog/2', '/blog/3', '/blog/4'],
+      dynamicRoutes: routes,
     })
   ],
   base: '/',
